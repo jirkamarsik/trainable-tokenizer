c260078e2b4a57425a5467de890410bdd2d2ce59
Jiri Marsik <jiri.marsik89@gmail.com>
2011-03-23 05:42:51 +0100

    Fixed issue no. 2
    Using function unicode_to_char on token's text property instead
    of the pretty_char_text function, which escapes newlines.

--- src/text_cleaner.cpp
+++ src/text_cleaner.cpp
@@ -11,7 +11,7 @@
  * or not. Since the preprocessors are defined using different types
  * in different namespaces, the code is repeated twice for both of them
  * through macros. */
-#define TEXTCLEANER(cleaner_namespace, cleaner_class, token_prefix) cleaner_namespace::cleaner_class lex(m_input_stream, m_input_encoding);\
+#define TEXTCLEANER(cleaner_namespace, cleaner_class, token_prefix, unicode_to_utf8) cleaner_namespace::cleaner_class lex(m_input_stream, m_input_encoding);\
 		lex.expand_entities = m_expand_entities;\
 		cleaner_namespace::Token *token_p = 0x0;\
 \
@@ -19,25 +19,25 @@
 			lex.receive(&token_p);\
 \
 			if (token_p->type_id() == token_prefix##TEXT) {\
-				*m_output_stream << token_p->pretty_char_text();\
+				*m_output_stream << unicode_to_utf8(token_p->get_text());\
 			}\
 			else if (token_p->type_id() == token_prefix##ENTITY) {\
 				if (m_expand_entities) {\
 					cutout_t cutout;\
 					cutout.kind = ENTITY;\
 					cutout.position = token_p->number;\
-					cutout.text = token_p->pretty_char_text();\
+					cutout.text = unicode_to_utf8(token_p->get_text());\
 					if (m_cutout_queue_p != 0x0)\
 						m_cutout_queue_p->push(cutout);\
-					*m_output_stream << expand_entity(token_p->pretty_char_text()); }\
+					*m_output_stream << expand_entity(unicode_to_utf8(token_p->get_text())); }\
 				else\
-					*m_output_stream << token_p->pretty_char_text();\
+					*m_output_stream << unicode_to_utf8(token_p->get_text());\
 			}\
 			else if (token_p->type_id() == token_prefix##XML) {\
 				cutout_t cutout;\
 				cutout.kind = XML;\
 				cutout.position = token_p->number;\
-				cutout.text = token_p->pretty_char_text();\
+				cutout.text = unicode_to_utf8(token_p->get_text());\
 				if (m_cutout_queue_p != 0x0)\
 					m_cutout_queue_p->push(cutout);\
 			}\
@@ -55,11 +55,11 @@ void TextCleaner::do_work()
 {
 	if (!m_hide_xml)
 	{
-		TEXTCLEANER(clean_entities, EntityCleaner, QUEX_PREPROC_NOXML_);
+		TEXTCLEANER(clean_entities, EntityCleaner, QUEX_PREPROC_NOXML_, clean_entities::EntityCleaner_unicode_to_char);
 	}
 	else
 	{
-		TEXTCLEANER(clean_xml, XmlCleaner, QUEX_PREPROC_WITHXML_);
+		TEXTCLEANER(clean_xml, XmlCleaner, QUEX_PREPROC_WITHXML_, clean_entities::EntityCleaner_unicode_to_char);
 	}
 }
 
