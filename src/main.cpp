c4a576bef90cf1ad751743dd85110693f90e2eb8
Jiri Marsik <jiri.marsik89@gmail.com>
2011-07-27 14:25:09 +0200

    OutputFormatter time reported only when present

--- src/main.cpp
+++ src/main.cpp
@@ -1049,8 +1049,10 @@ int main(int argc, char const **argv) {
       << endl;
     clog << "  Time spent actually classifying in Classifier: "
       << classifier_p->m_time_spent_classifying << endl;
-    clog << "Time spent in OutputFormatter: " << output_formatter_p->m_time_spent
-      << endl;
+    if (mode == TOKENIZE_MODE || mode == PREPARE_MODE) {
+      clog << "Time spent in OutputFormatter: " << output_formatter_p->m_time_spent
+        << endl;
+    }
 
     clog << endl;
     clog << endl;
